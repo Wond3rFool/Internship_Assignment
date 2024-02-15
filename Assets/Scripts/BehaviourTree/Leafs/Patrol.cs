61ce29ebd387522f186175e2af5fe130c2cacf0d
Tiago V <T.Verstoep96@gmail.com>
2024-02-15 10:53:24 +0100

    Patrolling enemy
    enemy now patrols around in a random direction and a random time frame

--- Assets/Scripts/BehaviourTree/Leafs/Patrol.cs
+++ Assets/Scripts/BehaviourTree/Leafs/Patrol.cs
@@ -2,26 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Patrol : Node
+public class Patrol: Node
 {
-	private int currentWayPointIndex = 0;
 
 	private float waitTime = 1f; // in seconds
 	private float waitCounter = 0f;
 	private bool waiting = false;
+
+	private Transform transform;
+	private Vector3 originPosition;
+	private Vector3 nextWaypointPosition;
+
+	private float moveSpeed;
+	private float patrolRadius;
+	public Patrol(Transform source, float moveSpeed, float patrolRadius)
+	{ 
+		transform = source;
+		originPosition = transform.position;
+		nextWaypointPosition = originPosition;
+		this.moveSpeed = moveSpeed;
+		this.patrolRadius = patrolRadius;
+	}
 
 	public override NodeState Evaluate()
 	{
 		if(waiting)
 		{
 			waitCounter += Time.deltaTime;
-			if(waitCounter >= waitTime)
+			if(waitCounter > waitTime)
 			{
 				waiting = false;
+				waitTime = Random.Range(0.1f, waitTime + 2);
 			}
-			return NodeState.RUNNING;
 		}
-		return NodeState.FAILED;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-	}
+		else
+		{
+			if(Vector3.Distance(transform.position, nextWaypointPosition) < 0.3f)
+			{
+				transform.position = nextWaypointPosition;
+				waitCounter = 0f;
+				waiting = true;
 
+				Vector2 randomPoint = Random.insideUnitCircle.normalized * patrolRadius;
+				nextWaypointPosition = originPosition + new Vector3(randomPoint.x, randomPoint.y, 0);
+			}
+			else
+			{
+				Vector3 direction = nextWaypointPosition - transform.position;
+				transform.position += direction.normalized * moveSpeed * Time.fixedDeltaTime;
+			}
+		}
+		return NodeState.RUNNING;
+	} 
 }
--- Assets/Scripts/Enemy/PatrolOfficer.cs
+++ Assets/Scripts/Enemy/PatrolOfficer.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PatrolOfficer : BehaviourTree
+public class PatrolOfficer: BehaviourTree
 {
 	public Transform[] waypoints;
 
 	[SerializeField]
 	private float speed;
 
+	[SerializeField]
+	[Range(1.0f, 15.0f)]
+	private float patrolRadius;
+
 	protected override Node SetupTree()
 	{
-		throw new System.NotImplementedException();
+		Node root = new Selector(new List<Node>
+		{
+			new Patrol(transform, speed, patrolRadius)
+	
+		});
+
+
+		return root;
 	}
 }
